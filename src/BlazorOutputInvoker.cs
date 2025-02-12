ad16fafab6bba5ac29dc931b4a932303853c9776
Jake Soenneker <jake@soenneker.com>
2025-02-12 15:33:18 -0600

    Removal of CT

--- src/BlazorOutputInvoker.cs
+++ src/BlazorOutputInvoker.cs
@@ -3,28 +3,28 @@ using System.Diagnostics.CodeAnalysis;
 using System.Threading.Tasks;
 using System;
 using Microsoft.JSInterop;
-using System.Threading;
 
 namespace Soenneker.Blazor.Utils.BlazorOutputInvoker;
 
 ///<inheritdoc cref="IBlazorOutputInvoker{TInput,TOutput}"/>
 public class BlazorOutputInvoker<TInput, TOutput> : IBlazorOutputInvoker<TInput, TOutput>
 {
-    private readonly Func<TInput, CancellationToken, ValueTask<TOutput>> _func;
+    private readonly Func<TInput, ValueTask<TOutput>> _func;
 
     /// <summary>
     /// Initializes a new instance of the <see cref="BlazorOutputInvoker{TInput,TOutput}"/> class.
     /// </summary>
     /// <param name="invoker">The invoker function.</param>
     [DynamicDependency(nameof(InvokeWithOutput))]
-    public BlazorOutputInvoker(Func<TInput, CancellationToken, ValueTask<TOutput>> invoker)
+    public BlazorOutputInvoker(Func<TInput, ValueTask<TOutput>> invoker)
     {
         _func = invoker;
     }
 
+    // CancellationToken cannot be a parameter due to this being called from JS
     [JSInvokable(nameof(InvokeWithOutput))]
-    public ValueTask<TOutput> InvokeWithOutput(TInput args, CancellationToken cancellationToken = default)
+    public ValueTask<TOutput> InvokeWithOutput(TInput args)
     {
-        return _func(args, cancellationToken);
+        return _func(args);
     }
 }
